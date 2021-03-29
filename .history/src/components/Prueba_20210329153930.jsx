98b2cc96d78f0667c36aa92b6e14d94f64712aa1
Jorge Andres Ayala Lopez <jorgi710@hotmail.com>
2021-03-29 15:40:39 -0500

    nuevooo

---
+++ .history/src/components/Prueba_20210329153930.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Prueba = () => {
+    return (
+        <div>
+            <h2>Hola Mundo</h2>
+            <button>Click</button>
+            <h2>prueba</h2>
+            <table>
+                <p>table</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda non tempore quo!</p>
+                <div className="btn btn-danger">
+                    <h2>click</h2>
+                </div>
+                <div className="container">
+                    <h2>darle click</h2>
+                </div>
+                <div className="container">
+                    <h2>hola</h2>
+                </div>
+                <h1>nuevo</h1>
+            </table>
+        </div>
+    )
+}
+
+export default Prueba
+ 
